3d2ee56aca1071afe2b11bf479b5c7a5108a208b
Hsooni <h.baalawi@outlook.com>
2025-09-29 16:07:35 +0300

    cleaning all the columns with converting datatypes

--- etl/clean_sales.py
+++ etl/clean_sales.py
@@ -7,18 +7,22 @@ df = pd.read_csv("data/dirty_cafe_sales.csv")
 df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
 
 # remove rows with missing values from item and transaction_id column
-df = df.dropna(subset=['item', 'transaction_id'])
+df = df.dropna(subset=['item', 'transaction_id', 'transaction_date'])
 
 # replacing missing values with NaNs
 df['transaction_date'].replace('UNKNOWN', np.nan, inplace=True)
 df['payment_method'].replace(['ERROR', 'UNKNOWN'], np.nan, inplace=True)
 df['item'].replace(['UNKNOWN', 'ERROR'], np.nan, inplace=True)
 df['quantity'].replace(['UNKNOWN', 'ERROR'], np.nan, inplace=True)
+df['total_spent'].replace(['UNKNOWN', 'ERROR'], np.nan, inplace=True)
+df['price_per_unit'].replace(['UNKNOWN', 'ERROR'], np.nan, inplace=True)
+df['location'].replace(['UNKNOWN', 'ERROR'], np.nan, inplace=True)
+df['transaction_date'].replace(['UNKNOWN', 'ERROR'], np.nan, inplace=True)
+
+# converting data type 
+df['quantity'] = df['quantity'].astype('Int64')
 df['item'] = df['item'].astype('category')
+df['total_spent'] = df['total_spent'].astype('float64')
+df['price_per_unit'] = df['total_spent'].astype('float64')
+df['transaction_date'] = pd.to_datetime(df['transaction_date'])
 
-
-# This line cause a ValueError, 'cannot convert float Nan to int' (I'll edit it later)
-df['quantity'] = df['quantity'].astype('int')
-
-
-print(df['quantity'].unique())
