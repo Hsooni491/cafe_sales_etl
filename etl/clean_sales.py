815149b78b659ba163ed9a97096f9adbc7e5cda3
Hsooni <h.baalawi@outlook.com>
2025-09-29 15:14:07 +0300

    clean missing values and adding some comments in the code

--- etl/clean_sales.py
+++ etl/clean_sales.py
@@ -3,10 +3,14 @@ import pandas as pd
 
 df = pd.read_csv("data/dirty_cafe_sales.csv")
 
+# strip and rename the column names
 df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
 
 df = df.dropna(subset=['item', 'transaction_id'])
 
+# replacing missing values with NaNs
 df['transaction_date'].replace('UNKNOWN', np.nan, inplace=True)
+df['payment_method'].replace('ERROR', np.nan, inplace=True)
+df['payment_method'].replace('UNKNOWN', np.nan, inplace=True)
 
-print(df[df['transaction_date'].isna() == True])
+print(df['payment_method'].unique())
