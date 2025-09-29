42c13e6595f4a32a3edb8d54fd6b39058ccfd295
Hsooni <h.baalawi@outlook.com>
2025-09-29 16:12:55 +0300

    replacing missing values before removing rows with missing values, by just taking the line of removing rows down

--- etl/clean_sales.py
+++ etl/clean_sales.py
@@ -6,9 +6,6 @@ df = pd.read_csv("data/dirty_cafe_sales.csv")
 # strip and rename the column names
 df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
 
-# remove rows with missing values from item and transaction_id column
-df = df.dropna(subset=['item', 'transaction_id', 'transaction_date'])
-
 # replacing missing values with NaNs
 df['transaction_date'].replace('UNKNOWN', np.nan, inplace=True)
 df['payment_method'].replace(['ERROR', 'UNKNOWN'], np.nan, inplace=True)
@@ -19,6 +16,9 @@ df['price_per_unit'].replace(['UNKNOWN', 'ERROR'], np.nan, inplace=True)
 df['location'].replace(['UNKNOWN', 'ERROR'], np.nan, inplace=True)
 df['transaction_date'].replace(['UNKNOWN', 'ERROR'], np.nan, inplace=True)
 
+# remove rows with missing values from item and transaction_id column
+df = df.dropna(subset=['transaction_id', 'item', 'quantity', 'transaction_date'])
+
 # converting data type 
 df['quantity'] = df['quantity'].astype('Int64')
 df['item'] = df['item'].astype('category')
@@ -26,3 +26,4 @@ df['total_spent'] = df['total_spent'].astype('float64')
 df['price_per_unit'] = df['total_spent'].astype('float64')
 df['transaction_date'] = pd.to_datetime(df['transaction_date'])
 
+print(df)
