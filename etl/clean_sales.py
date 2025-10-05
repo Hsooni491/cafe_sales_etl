722d0c58791c3c4b5ec37dbda1eddcc7117738dc
Hsooni <h.baalawi@outlook.com>
2025-10-05 20:48:27 +0300

    Clean the clean_sales.py script to remove redundant code and improve efficiency.

--- etl/clean_sales.py
+++ etl/clean_sales.py
@@ -1,37 +1,50 @@
 import numpy as np
 import pandas as pd
 
-def extract_csv(file_name):
+def extract_csv(file_name: str) -> pd.DataFrame:
+	'''Reads a CSV file and returns a Dataframe'''
 	df = pd.read_csv(file_name)
 	return df
 
+
+def clean_cafe_sales(df: pd.DataFrame) -> pd.DataFrame:
+	'''Cleans and preprocesses the cafe sales data'''
+
+	# strip and rename the column names
+	df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
+
+
+	# replacing missing values with NaNs
+	cols_to_clean = [
+		'transaction_date', 'payment_method', 'item', 'quantity',
+		'total_spent', 'price_per_unit', 'location', 'transaction_date'
+		]
+	for col in cols_to_clean:
+		df[col].replace(['UNKNOWN', 'ERROR'], np.nan, inplace=True)
+
+
+	# remove rows with missing values from the following columns
+	df = df.dropna(subset=[
+		'transaction_id', 'item', 'quantity', 'transaction_date', 'price_per_unit'
+		])
+	
+	# converting data type 
+	df = df.astype({
+		'quantity': 'Int64',
+		'item': 'category',
+		'total_spent': 'float64',
+		'payment_method': 'category',
+		'location': 'category'
+	})
+	df['transaction_date'] = pd.to_datetime(df['transaction_date'])
+
+	# creating a new column 'day_of_week' from 'transaction_date'
+	df['day_of_week'] = df['transaction_date'].dt.day_name().astype('category')
+
+	return df
+
+# --- Main script ----
 df = extract_csv('data/dirty_cafe_sales.csv')
-
-# strip and rename the column names
-df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
-
-# replacing missing values with NaNs
-df['transaction_date'].replace('UNKNOWN', np.nan, inplace=True)
-df['payment_method'].replace(['ERROR', 'UNKNOWN'], np.nan, inplace=True)
-df['item'].replace(['UNKNOWN', 'ERROR'], np.nan, inplace=True)
-df['quantity'].replace(['UNKNOWN', 'ERROR'], np.nan, inplace=True)
-df['total_spent'].replace(['UNKNOWN', 'ERROR'], np.nan, inplace=True)
-df['price_per_unit'].replace(['UNKNOWN', 'ERROR'], np.nan, inplace=True)
-df['location'].replace(['UNKNOWN', 'ERROR'], np.nan, inplace=True)
-df['transaction_date'].replace(['UNKNOWN', 'ERROR'], np.nan, inplace=True)
-
-# remove rows with missing values from the following columns
-df = df.dropna(subset=['transaction_id', 'item', 'quantity', 'transaction_date', 'price_per_unit'])
-
-# converting data type 
-df['quantity'] = df['quantity'].astype('Int64')
-df['item'] = df['item'].astype('category')
-df['total_spent'] = df['total_spent'].astype('float64')
-df['transaction_date'] = pd.to_datetime(df['transaction_date'])
-df['payment_method'] = df['payment_method'].astype('category')
-df['location'] = df['location'].astype('category')
-
-# creating a new column 'day_of_week' from 'transaction_date'
-df['day_of_week'] = df['transaction_date'].dt.day_name().astype('category')
+df = clean_cafe_sales(df)
 
 print(df.head())
