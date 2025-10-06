b55ff445d19ff573722a6537b08b86c1db5812da
Hsooni <h.baalawi@outlook.com>
2025-10-06 11:39:29 +0300

    create a logs directory and modify clean_sales.py to log messages to pipeline.log

--- etl/clean_sales.py
+++ etl/clean_sales.py
@@ -1,10 +1,24 @@
 import sys
 from pathlib import Path
+
 # --- Dynamically add project root to sys.path (must come before imports) ---
 root_path = Path(__file__).resolve().parent.parent
 sys.path.append(str(root_path))
+
 from utils.pipeline_utils import extract_csv, clean_cafe_sales
 import logging
+
+logs_dir = root_path / "logs"                       # this points to your logs folder
+log_file = logs_dir / "pipeline.log"                # this is the log file path
+
+logging.basicConfig(
+	level=logging.INFO,
+	format="%(asctime)s - %(levelname)s - %(message)s",
+	handlers=[
+		logging.StreamHandler(),
+		logging.FileHandler(log_file, mode='a')
+	]
+)
 
 # --- Main script ----
 try:
